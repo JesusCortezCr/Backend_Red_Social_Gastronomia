9f6e3fd17fd6df1166462b9fe54efab9538d63d9
Cortez Jesus <cortez_laloramos@hotmail.com>
2025-10-14 14:29:26 -0500

    feat(condicional rol) was created

--- src/main/java/com/app/backend_web/BackendWebApplication.java
+++ src/main/java/com/app/backend_web/BackendWebApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.app.backend_web.entities.Rol;
 import com.app.backend_web.repositories.RolRepository;
 
-import lombok.RequiredArgsConstructor;
 
 @SpringBootApplication
 public class BackendWebApplication implements CommandLineRunner {
@@ -24,15 +23,19 @@ public class BackendWebApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		Rol rolAdministrador = new Rol();
-		rolAdministrador.setNombre("ROLE_ADMINISTRADOR");
-		rolRepository.save(rolAdministrador);
-		Rol rolUsuario = new Rol();
-		rolUsuario.setNombre("ROLE_USUARIO");
-		rolRepository.save(rolUsuario);
-		Rol rolModerador = new Rol();
-		rolModerador.setNombre("ROLE_MODERADOR");
-		rolRepository.save(rolModerador);
+		if(rolRepository.findAll().isEmpty()){
+			Rol rolAdministrador = new Rol();
+			rolAdministrador.setNombre("ROLE_ADMINISTRADOR");
+			rolRepository.save(rolAdministrador);
+			Rol rolUsuario = new Rol();
+			rolUsuario.setNombre("ROLE_USUARIO");
+			rolRepository.save(rolUsuario);
+			Rol rolModerador = new Rol();
+			rolModerador.setNombre("ROLE_MODERADOR");
+			rolRepository.save(rolModerador);
+		}
+		System.out.println("ROLES CREADOS EXITOSAMENTE...");
+
 
 	}
 

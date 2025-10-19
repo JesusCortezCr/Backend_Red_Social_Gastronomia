b73d3483571e4ce9ae489c105960f5f1a6d96f95
nxt50 <nxtnxt507@gmail.com>
2025-10-19 13:09:28 -0500

    Add files via upload

--- src/main/java/com/app/backend_web/controllers/ComentarioController.java
+++ src/main/java/com/app/backend_web/controllers/ComentarioController.java
@@ -22,13 +22,11 @@ public class ComentarioController {
 
     private final ComentarioService comentarioService;
 
-    // --- LEER (TODOS) ---
     @GetMapping
     public ResponseEntity<?> traerComentarios() {
         return ResponseEntity.ok().body(comentarioService.listadoComentarios());
     }
 
-    // --- LEER (POR ID) ---
     @GetMapping("/{id}")
     public ResponseEntity<?> traerComentarioPorId(@PathVariable Long id) {
         if (comentarioService.buscarComentarioPorId(id).isPresent()) {
@@ -37,27 +35,21 @@ public class ComentarioController {
         return ResponseEntity.notFound().build();
     }
 
-    // --- INSERTAR (CREAR) ---
     @PostMapping
     public ResponseEntity<?> crearComentario(@RequestBody Comentario comentario) {
-        // Llama al servicio para que guarde el comentario en la base de datos.
         Comentario nuevoComentario = comentarioService.crearComentario(comentario);
         return ResponseEntity.status(201).body(nuevoComentario);
     }
 
-    // --- MODIFICAR (ACTUALIZAR) ---
     @PutMapping("/{id}")
     public ResponseEntity<?> modificarComentario(@PathVariable Long id, @RequestBody Comentario comentario) {
-        // Verificamos si el comentario existe antes de intentar actualizarlo
         if (comentarioService.buscarComentarioPorId(id).isPresent()) {
-            // El servicio se encargará de actualizar el comentario con el ID proporcionado
             Comentario comentarioActualizado = comentarioService.actualizarComentario(id, comentario);
             return ResponseEntity.ok(comentarioActualizado);
         }
         return ResponseEntity.notFound().build();
     }
 
-    // --- ELIMINAR ---
     @DeleteMapping("/{id}")
     ResponseEntity<?> eliminarComentarioPorId(@PathVariable Long id) {
         comentarioService.eliminarComentario(id);
